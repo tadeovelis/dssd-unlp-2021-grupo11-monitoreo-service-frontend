547996cbfc3075b2488651e8acbecdfd27856edc
tadeovelis <tadeovelis@gmail.com>
2021-11-12 23:38:43 -0300

    Agregar llamadas a la API para los listados de casos archivados y activos y los totales

--- src/components/ListaActiveCases.js
+++ src/components/ListaActiveCases.js
@@ -1,19 +1,93 @@
-import { Grid, Typography } from "@mui/material";
+import { Grid, Table, TableBody, TableCell, tableCellClasses, TableHead, TableRow, Typography } from "@mui/material";
 import { Box } from "@mui/system";
-import ListaCasos from "./ListaCasos";
+import { styled } from '@mui/material/styles';
+
+import env from "@beam-australia/react-env";
+import { formatDate, getCookie } from "../helpers/helpers";
+import { useEffect, useState } from "react";
+
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+        backgroundColor: "#393939",
+        color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+        fontSize: 14,
+    },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+        backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    '&:last-child td, &:last-child th': {
+        border: 0,
+    },
+}));
 
 
 export default function ListaActiveCases(props) {
 
-    function createData(id, fecha_inicio, fecha_finalizacion) {
-        return { id, fecha_inicio, fecha_finalizacion };
+    const [casosActivos, setCasosActivos] = useState([]);
+    const [cantCasosActivos, setCantCasosActivos] = useState(null);
+    const [rows, setRows] = useState([]);
+
+    function createData(id, fecha_inicio, fecha_ultima_actualizacion) {
+        return { id, fecha_inicio, fecha_ultima_actualizacion };
     }
 
-    const rows = [
-        createData(1, "1/10/2021", "1/10/2021"),
-        createData(2, "10/12/2021", "4/11/2021"),
-        createData(2, "20/03/2021", "18/10/2021"),
-    ];
+    // Obtener casos activos
+    useEffect(() => {
+        let ruta = "api/activeCase";
+
+        fetch(env("BACKEND_URL") + ruta, {
+            method: 'GET',
+            credentials: 'include',
+            /*
+            headers: {
+                'Authorization': 'Bearer ' + getCookie("access_token")
+            }
+            */
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                setCasosActivos(data);
+            })
+            .catch(error => console.error(error));
+    }, [])
+
+    // Obtener cant de casos activos
+    useEffect(() => {
+        let ruta = "api/activeCaseCount";
+
+        fetch(env("BACKEND_URL") + ruta, {
+            method: 'GET',
+            credentials: 'include',
+            /*
+            headers: {
+                'Authorization': 'Bearer ' + getCookie("access_token")
+            }
+            */
+        })
+            .then(response => response.json())
+            .then(data => {
+                setCantCasosActivos(data);
+            })
+            .catch(error => console.error(error));
+    }, [])
+
+    // Actualizar tabla cuando se actualicen los casos activos
+    useEffect(() => {
+        let rows = [];
+        casosActivos.map((c) => {
+            rows.push(createData(c.id, formatDate(c.start), formatDate(c.last_update_date)))
+        });
+        setRows(rows);
+    }, [casosActivos])
+
 
     return (
         <Box>
@@ -29,13 +103,34 @@ export default function ListaActiveCases(props) {
                     <Typography
                         variant="subtitle1"
                     >
-                        Total: <b>3</b>
+                        Total: <b>{cantCasosActivos}</b>
                     </Typography>
                 </Grid>
             </Grid>
-            <ListaCasos
-                rows={rows}
-            />
+
+            <Table sx={{ minWidth: 500 }}>
+                <TableHead>
+                    <TableRow>
+                        <StyledTableCell>ID</StyledTableCell>
+                        <StyledTableCell align="right">Fecha de inicio</StyledTableCell>
+                        <StyledTableCell align="right">Fecha de última actualización</StyledTableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {rows.map((row) => (
+                        <StyledTableRow
+                            key={row.id}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        >
+                            <StyledTableCell component="th" scope="row">
+                                {row.id}
+                            </StyledTableCell>
+                            <StyledTableCell align="right">{row.fecha_inicio}</StyledTableCell>
+                            <StyledTableCell align="right">{row.fecha_ultima_actualizacion}</StyledTableCell>
+                        </StyledTableRow>
+                    ))}
+                </TableBody>
+            </Table>
         </Box>
     )
 }
--- src/components/ListaArchivedCases.js
+++ src/components/ListaArchivedCases.js
@@ -1,19 +1,91 @@
-import { Grid, Typography } from "@mui/material";
+import { Grid, Table, TableBody, TableCell, tableCellClasses, TableHead, TableRow, Typography } from "@mui/material";
 import { Box } from "@mui/system";
-import ListaCasos from "./ListaCasos";
+import { styled } from '@mui/material/styles';
 
+import env from "@beam-australia/react-env";
+import { formatDate, getCookie } from "../helpers/helpers";
+import { useEffect, useState } from "react";
+
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+        backgroundColor: "#393939",
+        color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+        fontSize: 14,
+    },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+        backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    '&:last-child td, &:last-child th': {
+        border: 0,
+    },
+}));
 
 export default function ListaArchivedCases(props) {
 
-    function createData(id, fecha_inicio, fecha_finalizacion) {
-        return { id, fecha_inicio, fecha_finalizacion };
+    const [casosArchivados, setCasosArchivados] = useState([]);
+    const [cantCasosArchivados, setCantCasosArchivados] = useState(null);
+    const [rows, setRows] = useState([]);
+
+    function createData(id, fecha_inicio, fecha_finalizacion, fecha_ultima_actualizacion) {
+        return { id, fecha_inicio, fecha_finalizacion, fecha_ultima_actualizacion };
     }
 
-    const rows = [
-        createData(1, "1/10/2021", "1/10/2021"),
-        createData(2, "10/12/2021", "4/11/2021"),
-        createData(2, "20/03/2021", "18/10/2021"),
-    ];
+    // Obtener casos activos
+    useEffect(() => {
+        let ruta = "api/archivedCase";
+
+        fetch(env("BACKEND_URL") + ruta, {
+            method: 'GET',
+            credentials: 'include',
+            /*
+            headers: {
+                'Authorization': 'Bearer ' + getCookie("access_token")
+            }
+            */
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                setCasosArchivados(data);
+            })
+            .catch(error => console.error(error));
+    }, [])
+
+    // Obtener cant de casos activos
+    useEffect(() => {
+        let ruta = "api/archivedCaseCount";
+
+        fetch(env("BACKEND_URL") + ruta, {
+            method: 'GET',
+            credentials: 'include',
+            /*
+            headers: {
+                'Authorization': 'Bearer ' + getCookie("access_token")
+            }
+            */
+        })
+            .then(response => response.json())
+            .then(data => {
+                setCantCasosArchivados(data);
+            })
+            .catch(error => console.error(error));
+    }, [])
+
+    // Actualizar tabla cuando se actualicen los casos activos
+    useEffect(() => {
+        let rows = [];
+        casosArchivados.map((c) => {
+            rows.push(createData(c.id, formatDate(c.start), formatDate(c.end_date), formatDate(c.last_update_date)))
+        });
+        setRows(rows);
+    }, [casosArchivados])
 
     return (
         <Box>
@@ -29,13 +101,36 @@ export default function ListaArchivedCases(props) {
                     <Typography
                         variant="subtitle1"
                     >
-                        Total: <b>3</b>
+                        Total: <b>{cantCasosArchivados}</b>
                     </Typography>
                 </Grid>
             </Grid>
-            <ListaCasos
-                rows={rows}
-            />
+
+            <Table sx={{ minWidth: 500 }}>
+                <TableHead>
+                    <TableRow>
+                        <StyledTableCell>ID</StyledTableCell>
+                        <StyledTableCell align="right">Fecha de inicio</StyledTableCell>
+                        <StyledTableCell align="right">Fecha de finalización</StyledTableCell>
+                        <StyledTableCell align="right">Fecha de última actualización</StyledTableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {rows.map((row) => (
+                        <StyledTableRow
+                            key={row.id}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        >
+                            <StyledTableCell component="th" scope="row">
+                                {row.id}
+                            </StyledTableCell>
+                            <StyledTableCell align="right">{row.fecha_inicio}</StyledTableCell>
+                            <StyledTableCell align="right">{row.fecha_finalizacion}</StyledTableCell>
+                            <StyledTableCell align="right">{row.fecha_ultima_actualizacion}</StyledTableCell>
+                        </StyledTableRow>
+                    ))}
+                </TableBody>
+            </Table>
         </Box>
     )
 }
